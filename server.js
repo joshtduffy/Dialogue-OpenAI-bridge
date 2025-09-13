c4efa4342599f933207c1bafc973734470fc8da9
joshtduffy <joshtduffy@gmail.com>
2025-09-13 01:36:05 -0500

    Update server.js
    Signed-off-by: joshtduffy <joshtduffy@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
